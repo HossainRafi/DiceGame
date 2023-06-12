f24155ca8273f7027950eea5e10e0118c2fdf824
HossainRafi <hossainrafi71@gmail.com>
2023-06-12 21:32:31 +0600

    img

--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -1,8 +1,21 @@
+import styled from "styled-components";
 
 const StartGame = () => {
   return (
-    <div>StartGame</div>
-  )
-}
+    <Container>
+      <div>
+        <img src="/images/dices.png" />
+      </div>
+    </Container>
+  );
+};
 
-export default StartGame
+export default StartGame;
+
+const Container = styled.div`
+  max-width: 1180px;
+  height: 100vh;
+  display: flex;
+  margin: 0 auto;
+  align-items: center;
+`;
