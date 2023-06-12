df40c5cf3c049cfa3c2230984f6aa044f3f8b66f
HossainRafi <hossainrafi71@gmail.com>
2023-06-12 22:06:14 +0600

    create function for btn toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import StartGame from "./components/StartGame";
 
 function App() {
+  const [isGameStarted, setIsGameStarted] = useState(false);
+  const toggleGamePlay = () => {
+    setIsGameStarted((prev) => !prev);
+  };
+
   return (
     <>
       <StartGame />
