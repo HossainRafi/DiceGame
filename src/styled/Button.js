9db5581ea0930ccad41175105b68794ea3cfb64c
HossainRafi <hossainrafi71@gmail.com>
2023-06-12 21:57:37 +0600

    outline btn hover

--- src/styled/Button.js
+++ src/styled/Button.js
@@ -22,4 +22,9 @@ export const OutlineButton = styled(Button)`
   background-color: white;
   border: 1px solid black;
   color: black;
+  &:hover {
+    background-color: black;
+    border: 1px solid transparent;
+    color: white;
+  }
 `;
