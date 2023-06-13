7a541b27e5e336200f45bdf53bc4a0725e7b1707
HossainRafi <hossainrafi71@gmail.com>
2023-06-13 21:32:27 +0600

    maincontainer

--- src/components/GamePlay.jsx
+++ src/components/GamePlay.jsx
@@ -1,13 +1,34 @@
 import NumberSelector from "./NumberSelector";
+import styled from "styled-components";
 import TotalScore from "./TotalScore";
 
 const GamePlay = () => {
   return (
-    <main>
-      <TotalScore/>
-      <NumberSelector/>
-    </main>
+    <MainContainer>
+      <div className="top_section">
+        <TotalScore />
+        <NumberSelector />
+      </div>
+    </MainContainer>
   );
 };
 
 export default GamePlay;
+
+const MainContainer = styled.main`
+  padding-top: 70px;
+  .top_section {
+    display: flex;
+    justify-content: space-around;
+    align-items: end;
+  }
+  .btns {
+    margin-top: 40px;
+    gap: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+  }
+`;
