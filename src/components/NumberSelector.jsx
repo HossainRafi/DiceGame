149c9d03a886c21803a8617b5757193676fc0f07
HossainRafi <hossainrafi71@gmail.com>
2023-06-13 21:27:44 +0600

    create design

--- src/components/NumberSelector.jsx
+++ src/components/NumberSelector.jsx
@@ -1,17 +1,35 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 const NumberSelector = () => {
   const arrNumber = [1, 2, 3, 4, 5, 6];
+  const [selectedNumber, setSelectedNumber] = useState();
+
   return (
-    <div>
-      {arrNumber.map((value, i) => (
-        <Box key={i}>{value}</Box>
-      ))}
-    </div>
+    <NumberSelectorContainer>
+      <div className="flex">
+        {arrNumber.map((value, i) => (
+          <Box
+            isSelected={value === selectedNumber}
+            key={i}
+            onClick={() => setSelectedNumber(value)}
+          >
+            {value}
+          </Box>
+        ))}
+      </div>
+      <p>Select Number</p>
+    </NumberSelectorContainer>
   );
 };
 
 export default NumberSelector;
+
+const NumberSelectorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: end;
+`;
 
 const Box = styled.div`
   height: 72px;
