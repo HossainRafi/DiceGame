26dba2d0dd4c0dc5f652ff60b3fed8f5894c78bd
HossainRafi <hossainrafi71@gmail.com>
2023-06-13 20:59:38 +0600

    create and design box

--- src/components/NumberSelector.jsx
+++ src/components/NumberSelector.jsx
@@ -1,5 +1,24 @@
+import styled from "styled-components";
+
 const NumberSelector = () => {
-  return <div></div>;
+  return <div>
+    <Box>
+      
+    </Box>
+  </div>;
 };
 
 export default NumberSelector;
+
+
+const Box = styled.div`
+  height: 72px;
+  width: 72px;
+  border: 1px solid black;
+  display: grid;
+  place-items: center;
+  font-size: 24px;
+  font-weight: 700;
+  background-color: ${(props) => (props.isSelected ? "black" : "white")};
+  color: ${(props) => (!props.isSelected ? "black" : "white")};
+`;
